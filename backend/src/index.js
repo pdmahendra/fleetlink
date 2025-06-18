c2d433e670cb31432b7e4083cd7bcc30ab3d7625
pdmahendra <pratikkhatri520@gmail.com>
2025-06-18 16:29:42 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,8 +9,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN_LOCAL, process.env.CORS_ORIGIN_PRODUCTION],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/v1", vehicleRoutes);
 
 app.use(errorHandler);
