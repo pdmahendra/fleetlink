37dfe9809a1a8cb4133b99a9dbff463d91bac5d1
pdmahendra <pratikkhatri520@gmail.com>
2025-06-18 16:19:06 +0530

    first commit

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://fleetlink.onrender.com/api/v1",
   // baseURL: "http://localhost:3001/api/v1",
   headers: {
     "Content-Type": "application/json",
